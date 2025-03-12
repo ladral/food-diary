3e03c3f3f86d4dff05414c5545da58315f9df618
ladral <philipp.tuor@ladral.ch>
2025-03-12 13:42:32 +0100

    chore: remove typos

--- apps/food-diary/src/App.tsx
+++ apps/food-diary/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">å©
+      <div className="card">
         <button className="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>

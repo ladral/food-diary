6388540f801e7934ce80348b6ff640c2243c6e77
ladral <philipp.tuor@ladral.ch>
2025-03-28 22:09:18 +0100

    feat: allow CORS requests

--- apps/food-diary-api/food_diary_api/settings.py
+++ apps/food-diary-api/food_diary_api/settings.py
@@ -36,12 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'diary',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,3 +131,5 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'https://localhost:8000',
 ]
+
+CORS_ALLOWED_ORIGINS = os.environ.get("FOOD_DIARY_API_CORS_ALLOWED_ORIGINS", default="").split(" ")

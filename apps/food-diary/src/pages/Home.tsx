6d3715c4247a6fa4d739ab443a0f8649e71946e7
ladral <philipp.tuor@ladral.ch>
2025-05-17 12:59:17 +0200

    feat: add user menu to avatar and move logout functionality to user menu

--- apps/food-diary/src/pages/Home.tsx
+++ apps/food-diary/src/pages/Home.tsx
@@ -1,32 +1,9 @@
-import useKeycloak from "../hooks/useKeycloak";
 import "./Home.scss";
 import FoodDiaryTable from "../components/list/FoodDiaryTable.tsx";
 
 const HomePage: React.FC = () => {
-    const { keycloak, authenticated } = useKeycloak();
-
-    const handleLogin = () => {
-        keycloak?.login();
-    };
-
-    const handleLogout = () => {
-        keycloak?.logout();
-    };
-
     return (
         <div>
-            <div className="user card is-flex is-flex-direction-column is-align-items-center">
-                {authenticated ? (
-                    <button className="user__logout fd-button fd-button--secondary" color="inherit" onClick={handleLogout}>
-                        Logout
-                    </button>
-                ) : (
-                    <button className="user__login fd-button fd-button--secondary" color="inherit" onClick={handleLogin}>
-                        Login
-                    </button>
-                )}
-            </div>
-
             <FoodDiaryTable/>
         </div>
     );

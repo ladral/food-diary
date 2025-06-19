9cd02412f8a08d3e00b85c7a0bf96174c9e178ce
ladral <philipp.tuor@ladral.ch>
2025-06-19 19:49:19 +0200

    build: use text compression to reduce asset load size

--- apps/food-diary/vite.config.ts
+++ apps/food-diary/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import compression from 'vite-plugin-compression';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    compression({
+      algorithm: 'gzip',
+      deleteOriginFile: false,
+    })
+  ]
 })
